c0d71805579f3c0f12ff32aaf0587f41a4aecbd2
DESKTOP-8M0J74Q\User <nwy140@gmail.com>
2018-01-08 16:34:14 +0800

    In Main Function, Decrypt Data then  Write Data to outfile. ofstream fo (out) //this is out file // for infile you use ifstream fi (in) Declare at header and define appropriate method to make DecryptB64 method work .

--- Decrypt/main.cpp
+++ Decrypt/main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-string DecryptB64();
+
+
+const std::string &BASE64_CODES = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+const std::string &SALT1 = "LM::TB::BB";
+const std::string &SALT2 = "__:/__77";
+const std::string &SALT3 = "line=wowC++";
+
+string DecryptB64(string s);
+string base64_decode(const std::string &s);
 
 int main( int argc , char *argv[])
 {
@@ -17,16 +26,19 @@ int main( int argc , char *argv[])
     string data ;
     fi >> data ; // input into data
     if (!fi) // if input is still empty
-        return cout << "Input file" << in <<"' corrupted!" <<endl, 4;
-    data = DecryptBase64 (data);
-
-
+        return cout << "Input file '" << in <<"' corrupted!" <<endl, 4;
+    data = DecryptB64 (data);
+    ofstream fo (out); // output to file
+    if (!fo)
+        return cout << "Cannot write output file '" << out << "'" <<endl , 5 ;
+    fo << data; // output data to file
+    cout << "Decoding was succesful" << endl;
 
     return 0;
 }
 
 
-string DecryptB64 (std::string s)
+string DecryptB64 (string s)
     {
         s = s.erase (7, 1);
         s = s.erase (1, 1);
@@ -40,4 +52,29 @@ string DecryptB64 (std::string s)
         s = s.substr (SALT1.length());
         s = s.substr (0, s.length() - SALT2.length() - SALT3.length());
         return s;
+    }
+
+
+string base64_decode(const std::string &s)
+    {
+        string ret;
+        vector<int> vec(256, -1);
+        for (int i = 0; i < 64; i++)
+            vec [BASE64_CODES[i]] = i;
+        int val = 0, bits = -8;
+        for (const auto &c : s)
+            {
+                if (vec[c] == -1) break;
+                val = (val << 6) + vec[c];
+                bits += 6;
+
+                if (bits >= 0)
+                    {
+
+                        ret.push_back(char((val >> bits) & 0xFF));
+                        bits -= 8;
+                    }
+            }
+
+        return ret;
     }
